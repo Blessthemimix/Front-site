911dd6fa45e7a467b5a900bc2c791ea2db4171dc
Blessthemimix <m1mix@outlook.com>
2026-04-02 13:39:58 +0300

    DOLBAEB fix

--- bot_app/web_app.py
+++ bot_app/web_app.py
@@ -12,7 +12,7 @@ import os
 from supabase import create_client, Client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY") or os.getenv("SUPABASE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 logger = logging.getLogger(__name__)
